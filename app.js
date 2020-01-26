546ef2622b5a1f2279c9fe5a800f44947d978e96
Zeng Tao <qingchun1995@gmail.com>
2020-01-26 19:41:34 +0800

    Add the error controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const menuRouter = require('./routes/menu')
+const errorController = require('./controllers/error.js')
 
 const app = express()
 
@@ -21,19 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(menuRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+app.use(errorController.get404)
 
 module.exports = app
