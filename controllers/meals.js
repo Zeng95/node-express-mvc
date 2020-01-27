e8d49d7b61313c845231c2f90e5385cb3cf0f382
Zeng Tao <qingchun1995@gmail.com>
2020-01-27 16:55:09 +0800

    Fetch all meals data via the model
    fix the asynchronous problem

--- controllers/meals.js
+++ controllers/meals.js
@@ -2,10 +2,11 @@ const Meal = require('../models/meal')
 
 // Controller function
 exports.getMeals = (req, res, next) => {
-  const meals = Meal.fetchAll()
-  // Pass it the meals array to render (that's how you feed data into views)
-  res.render('menu', {
-    title: 'Menu Page',
-    meals
+  Meal.fetchAll((meals) => {
+    // Pass it the meals array to render (that's how you feed data into views)
+    res.render('menu', {
+      title: 'Menu Page',
+      meals
+    })
   })
 }
--- models/meal.js
+++ models/meal.js
@@ -1,12 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-const meals = [
-  { name: "MilkShake", type: "breakfast", price: 8, className: 'list-group-item-danger' },
-  { name: "Lazanya", type: "lunch", price: 20, className: 'list-group-item-warning' },
-  { name: "Roast Lamb", type: "dinner", price: 50, className: 'list-group-item-info' }
-]
-
 class Meal {
   constructor(name = '', type = '', price = '') {
     this.name = name
@@ -36,8 +30,21 @@ class Meal {
   }
 
   // Meal 类自身的方法
-  static fetchAll() {
-    return meals
+  static fetchAll(callback) {
+    const p = path.join(__basedir, 'data', 'meals.json')
+
+    fs.readFile(p, (err, data) => {
+      let meals
+      
+      // 如果 meals.json 存在
+      if (!err) {
+        meals = JSON.parse(data)
+      } else {
+        meals = []
+      }
+
+      callback(meals)
+    })
   }
 }
 
